992e37dd3b98885f5b0657d358fdac86440c9f39
rsoni1 <ritesh.soni@gmail.com>
2015-03-03 12:57:29 -0800

    Filtering out unwanted error messages in spark-stop Filtering out INFO messaging in submit job

--- scratch/runjob.sh
+++ scratch/runjob.sh
@@ -8,5 +8,5 @@ CURRDIR=$PWD
 
 source /global/home/groups/allhands/bin/spark_helper.sh
 spark-start
-spark-submit --master $SPARK_URL $CURRDIR/$1
-spark-stop
+spark-submit --master $SPARK_URL $CURRDIR/$1 2>&1 | grep -v INFO
+spark-stop 2>&1 | grep -v mkdir | grep -v chown
