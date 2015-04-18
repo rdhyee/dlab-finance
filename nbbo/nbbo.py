52da311416e50e7be2e2621634b045de05af3d57
rsoni1 <ritesh.soni@gmail.com>
2015-04-18 05:41:48 -0700

    Using zipWithIndex() to preserve original ordering in case to tied timestamps Using this index as the first position in the tuple to optimize the sorted() function defining the output of processquote as key value pairs for use for the crossings calculation stage

--- nbbo/nbbo.py
+++ nbbo/nbbo.py
@@ -32,12 +32,8 @@ inputDir="/global/scratch/rsoni/testdata/quoteplain/"
 outputDir="/global/scratch/rsoni/testdata/nbbo/"
 
 def processquote (record):
-    # Sort by timestamp in millisecond
-    list1 = sorted(record[1],key=lambda rec: rec[0])
-    
-    # Filter out bid/asks with value 0 or size 0 DO NOT FILTER HERE
-    # list1 = [v for v in sortlist if (v[3] != 0) & (v[4] != 0) & (v[5] != 0) & (v[6] != 0)]
-
+    # Sort by index created using zipWithIndex to preserve ordering in tied timestamps
+    list1 = sorted(record[1])
     # Setup exchangeList for NBBO calculation
     exchangeList = ['A','B','C','D','I','J','K','M','N','T','P','S','Q','W','X','Y','Z']
     bidList = [0]*len(exchangeList)
@@ -47,35 +43,40 @@ def processquote (record):
     for i in range(len(list1)):
         # set the latest bid and ask if bid & ask are not zero and if bidsize and asksize are not zero
         # Backout the bid or ask if either is 0
-        if ((list1[i][3] != 0) & (list1[i][4] != 0)):
-            bidList[exchangeList.index(list1[i][8])] = list1[i][3]
-        elif (list1[i][3] == 0):
-            bidList[exchangeList.index(list1[i][8])] = 0
-
-        if ((list1[i][5] != 0) & (list1[i][6] != 0)):
-            askList[exchangeList.index(list1[i][9])] = list1[i][5]
-        elif (list1[i][5] == 0):
-            askList[exchangeList.index(list1[i][9])] = sys.maxsize
-
+        if ((list1[i][4] != 0) & (list1[i][5] != 0)):
+            bidList[exchangeList.index(list1[i][9])] = list1[i][4]
+        elif ((list1[i][4] == 0) or (list1[i][11] == 'B')):
+            bidList[exchangeList.index(list1[i][9])] = 0
+        if ((list1[i][6] != 0) & (list1[i][7] != 0)):
+            askList[exchangeList.index(list1[i][10])] = list1[i][6]
+        elif ((list1[i][6] == 0) or (list1[i][11] == 'B')):
+            askList[exchangeList.index(list1[i][10])] = sys.maxsize
         # calculate NBBO
-        nbbolist.append((record[0],list1[i][0],max(bidList),min(askList)))
+        if (max(bidList) > 0) or (min(askList) < sys.maxsize):
+            # Output key Value pairs where
+            # Key : (<Stock Ticker>, <Time in seconds>)
+            # Value : (<record-index>,<bid-price>,<ask-price>,<best-bid>,<best-ask>)
+            nbbolist.append(((record[0],list1[i][2]),(list1[i][0],list1[i][4],list1[i][6],max(bidList),min(askList))))
     return nbbolist
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("nbbo_hfalert")
     sc = SparkContext(conf=conf)
-    data = sc.textFile(inputDir)
+    data1 = sc.textFile(inputDir)
+    data2 = data1.zipWithIndex()
     # data1 = data.filter(lambda line: line[10:26].strip() == 'AAPL')
-    biddata = data.map(lambda line: (line[10:26].strip(), 
-                                     (int(line[0:9]), 
-                                      int(line[0:6]), 
-                                      int(line[6:9]), 
-                                      float(line[26:37])/10000,
-                                      int(line[37:44]),
-                                      float(line[44:55])/10000,
-                                      int(line[55:62]),
-                                      line[9],
-                                      line[67],
-                                      line[68]))).groupByKey()
-    result = biddata.flatMap(lambda rec: processquote(rec))
+    data3 = data2.map(lambda rec: (rec[0][10:26].strip(), 
+                                   (rec[1],
+                                    int(rec[0][0:9]), 
+                                    int(rec[0][0:6]), 
+                                    int(rec[0][6:9]), 
+                                    float(rec[0][26:37])/10000,
+                                    int(rec[0][37:44]),
+                                    float(rec[0][44:55])/10000,
+                                    int(rec[0][55:62]),
+                                    rec[0][9],
+                                    rec[0][67],
+                                    rec[0][68],
+                                    rec[0][87]))).groupByKey()
+    result = data3.flatMap(lambda records: processquote(records))
     result.saveAsTextFile(outputDir)
