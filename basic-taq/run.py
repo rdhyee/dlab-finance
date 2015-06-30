2ab748cc166c6b2d9f0e470b632ff5ff64e08bff
Dav Clark <davclark@berkeley.edu>
2015-06-30 15:41:32 -0700

    Made run.py into a more general script

--- basic-taq/run.py
+++ basic-taq/run.py
@@ -1,10 +1,9 @@
-from glob import glob
+#!/usr/bin/env python3
+
 import raw_taq
 import pandas as pd
 import numpy as np
 from statistics import mode, StatisticsError
-from importlib import reload
-reload(raw_taq)
 
 def print_stats(chunk):
     #find the max bid price
@@ -29,32 +28,40 @@ def print_stats(chunk):
           "Mean bid price: ", avg_price, "\n", "Mod bid price: ", mod_price, "\n",
           "Standard deviation bid price: ", sd_price)
 
-# I grab the [0]'th fname in the glob
-fname = '../local_data/EQY_US_ALL_BBO_20150102.zip'
-local_taq = raw_taq.TAQ2Chunks(fname)
+def process_chunks(taq):
+    chunk_gen = taq.convert_taq(20)
+    first_chunk = next(chunk_gen)
+    curr_symbol = first_chunk['Symbol_root'][0]
 
-chunk_gen = local_taq.convert_taq(20)
-first_chunk = next(chunk_gen)
-curr_symbol = first_chunk['Symbol_root'][0]
+    accum = pd.DataFrame(first_chunk)
 
-accum = pd.DataFrame(first_chunk)
+    processed_symbols = 0
+    for chunk in chunk_gen:
+        where_symbol = curr_symbol == chunk['Symbol_root']
+        if where_symbol.all():
+            accum.append(pd.DataFrame(chunk))
+        else:
+            same = chunk[where_symbol]
+            accum.append(pd.DataFrame(same))
 
-processed_symbols = 0
-for chunk in chunk_gen:
-    where_symbol = curr_symbol == chunk['Symbol_root']
-    if where_symbol.all():
-        accum.append(pd.DataFrame(chunk))
-    else:
-        same = chunk[where_symbol]
-        accum.append(pd.DataFrame(same))
+            # Compute the stats
+            print('Current symbol:', curr_symbol, len(curr_symbol), 'records')
+            print_stats(accum)
+            processed_symbols += 1
+            if processed_symbols > 3:
+                break
 
-        # Compute the stats
-        print('Current symbol:', curr_symbol, len(curr_symbol), 'records')
-        print_stats(accum)
-        processed_symbols += 1
-        if processed_symbols > 3:
-            break
+            diff = chunk[~where_symbol]
+            accum = pd.DataFrame(diff)
+            curr_symbol = accum.Symbol_root[0]
 
-        diff = chunk[~where_symbol]
-        accum = pd.DataFrame(diff)
-        curr_symbol = accum.Symbol_root[0]
+if __name__ == '__main__':
+    # I grab the [0]'th fname in the glob
+    # fname = '../local_data/EQY_US_ALL_BBO_20150102.zip'
+    # fname = '../local_data/EQY_US_ALL_BBO_20140206.zip'
+    from sys import argv
+    fname = '../local_data/EQY_US_ALL_BBO_201501' + argv[1] + '.zip'
+    print("processing", fname)
+    local_taq = raw_taq.TAQ2Chunks(fname)
+
+    process_chunks(local_taq)
